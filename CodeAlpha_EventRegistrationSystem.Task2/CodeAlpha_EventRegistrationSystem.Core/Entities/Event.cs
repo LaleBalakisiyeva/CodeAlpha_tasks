f6e3259b66d73a29b2a1a4b456a9bf8b45192328
Lalə Balakişiyeva <lalebalakisiyeva258@gmail.com>
2026-09-07 10:53:37 +0400

    feat: add Task 2 folder and Core models

---
+++ CodeAlpha_EventRegistrationSystem.Task2/CodeAlpha_EventRegistrationSystem.Core/Entities/Event.cs
@@ -0,0 +1,19 @@
+﻿using CodeAlpha_EventRegistrationSystem.Core.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAlpha_EventRegistrationSystem.Core.Entities
+{
+    public class Event : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime EventDate { get; set; }
+        public string Location { get; set; }
+
+        public ICollection<Registration> Registrations { get; set; }
+    }
+}
---
+++ CodeAlpha_EventRegistrationSystem.Task2/CodeAlpha_EventRegistrationSystem.Core/Entities/Registration.cs
@@ -0,0 +1,18 @@
+﻿using CodeAlpha_EventRegistrationSystem.Core.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAlpha_EventRegistrationSystem.Core.Entities
+{
+    public class Registration : BaseEntity
+    {
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
