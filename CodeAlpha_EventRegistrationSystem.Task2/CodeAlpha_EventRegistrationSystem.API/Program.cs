99643cd10143f0fa7988cdd33b6ae2c046af738c
Lalə Balakişiyeva <lalebalakisiyeva258@gmail.com>
2026-09-08 10:24:41 +0400

    feat: implement Generic Repository, Unit of Work and configure DI for Task 2

--- CodeAlpha_EventRegistrationSystem.Task2/CodeAlpha_EventRegistrationSystem.API/Program.cs
+++ CodeAlpha_EventRegistrationSystem.Task2/CodeAlpha_EventRegistrationSystem.API/Program.cs
@@ -1,9 +1,14 @@
+using CodeAlpha_EventRegistrationSystem.DAL.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
---
+++ CodeAlpha_EventRegistrationSystem.Task2/CodeAlpha_EventRegistrationSystem.DAL/Repositories/Interfaces/IRegistrationRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAlpha_EventRegistrationSystem.DAL.Repositories.Interfaces
+{
+    internal class IRegistrationRepository
+    {
+    }
+}
