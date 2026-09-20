77ca50c47031bf891b60e5cd9e5369375e64e6f8
Lalə Balakişiyeva <lalebalakisiyeva258@gmail.com>
2026-09-20 15:12:07 +0400

    feat: implement generic and specific repositories with UnitOfWork in DAL

---
+++ CodeAlpha_RestaurantManagementSystem.Task3/CodeAlpha_RestaurantManagementSystem.DAL/Repositories/Interfaces/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAlpha_RestaurantManagementSystem.DAL.Repositories.Interfaces
+{
+    internal class IOrderRepository
+    {
+    }
+}
